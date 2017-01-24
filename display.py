61d0b2b7d17077d85172b777c987c676b7c7d269
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2017-01-24 13:02:36 -0600

    added empty word display

--- display.py
+++ display.py
@@ -5,3 +5,15 @@ class Display:
         print("Welcome to Hangman")
         print("Press 1 to start, 2 to exit")
 
+    @staticmethod
+    def show_game_board(word):
+
+        empty_word_string = ''
+
+        for i in word:
+
+            empty_word_string += '_ '
+
+        print("\n" + empty_word_string)
+
+        pass
