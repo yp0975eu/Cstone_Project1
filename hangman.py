71e23adbddce1298b344378686d392282c59b940
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2017-01-24 13:37:23 -0600

    added class structure to main class. added checking if letter in word

--- hangman.py
+++ hangman.py
@@ -1,56 +1,99 @@
 import display
 
 
-def start_game():
+class Main:
 
-    # dictionary from: https://www.flocabulary.com/5th-grade-vocabulary-word-list/
-    word = select_word()
+    word = None
+    file_name = 'words.txt'
 
-    display.Display.show_game_board(word)
+    def __init__(self):
 
+        self.word = self.select_word()
 
-# get menu option
-def select_menu_option():
+    def start_game(self):
 
-    selection = input("\n")
+        # dictionary from: https://www.flocabulary.com/5th-grade-vocabulary-word-list/
+        word = self.select_word()
 
-    while selection not in ['1', '2']:
+        display.Display.show_game_board(word)
+
+        self.game_loop()
+
+    def game_loop(self):
+        import string
+
+        game_status = True
+
+        # https://docs.python.org/3/library/string.html#string-constants
+
+        available_letters = list(string.ascii_lowercase)
+
+        guessed_letters = []
+
+        display.Display.show_available_letters(available_letters, guessed_letters)
+
+        while game_status:
+
+            display.Display.guess_letter_message()
+
+            guess = input('\n')
+
+            while guess not in available_letters:
+
+                guess = input('\nThat\'s not a valid guess.')
+
+            if guess in self.word:
+
+                print(guess)
+
+                print(self.word)
+
+            else:
+
+                print('bad guess')
+
+    # get menu option
+    def select_menu_option(self):
 
         selection = input("\n")
 
-    if selection == '1':
+        while selection not in ['1', '2']:
 
-        start_game()
+            selection = input("\n")
 
-    elif selection == '2':
+        if selection == '1':
 
-        exit()
+            self.start_game()
 
+        elif selection == '2':
 
-# selects random word from words.txt
-def select_word():
-    import random
-    i = 0
+            exit()
 
-    file_name = 'words.txt'
+    # selects random word from words.txt
+    def select_word(self):
+        import random
+        i = 0
 
-    # get line count
-    # http://stackoverflow.com/q/845058
-    with open(file_name) as f:
-        for i, l in enumerate(f):
-            pass
+        # get line count
+        # http://stackoverflow.com/q/845058
+        with open(self.file_name) as f:
+            for i, l in enumerate(f):
+                pass
 
-    file = open(file_name, "r")
+        file = open(self.file_name, "r")
 
-    lines = file.readlines()
+        lines = file.readlines()
 
-    return lines[random.randint(0, i)]
+        return lines[random.randint(0, i)]
 
+    # main function
+    def run(self):
 
-# main function
-def run():
-    display.Display.welcome_menu()
-    select_menu_option()
+        display.Display.welcome_menu()
+
+        self.select_menu_option()
+
 
 if __name__ == "__main__":
-    run()
+    m = Main()
+    m.run()
