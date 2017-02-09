7f1cf45ad2e69492efc1c32aa1ceedb12af9d83a
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2017-02-09 13:57:20 -0600

    refactor method names

--- display.py
+++ display.py
@@ -3,7 +3,7 @@ def welcome_menu():
     print("Press 1 to start, 2 to exit")
 
 
-def show_game_board(word):
+def game_board(word):
     empty_word_string = ''
 
     for i in word:
@@ -12,23 +12,21 @@ def show_game_board(word):
 
     print("\n" + empty_word_string.strip())
 
-    pass
-
 
 def guess_letter_message():
     print("\nGuess a letter")
 
 
-def show_available_letters(available_letters):
+def available_letters(letters):
     print('\n\t\t\t\tAvailable letters')
-    print(' '.join(available_letters))
+    print(' '.join(letters))
 
 
 def show_word(word):
     print(word)
 
 
-def show_correct_guessed_letters(word, guessed_letters):
+def correct_guessed_letters(word, guessed_letters):
     guess_string = ''
 
     for letter in word:
