077435a7cdbd25bae54884f6ded1658d6a9b20e9
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2017-02-09 12:58:17 -0600

    change Display class static methods to module methods

--- display.py
+++ display.py
@@ -1,45 +1,41 @@
-class Display:
+def welcome_menu():
+    print("Welcome to Hangman")
+    print("Press 1 to start, 2 to exit")
 
-    @staticmethod
-    def welcome_menu():
-        print("Welcome to Hangman")
-        print("Press 1 to start, 2 to exit")
 
-    @staticmethod
-    def show_game_board(word):
+def show_game_board(word):
+    empty_word_string = ''
 
-        empty_word_string = ''
+    for i in word:
 
-        for i in word:
+        empty_word_string += '_ '
 
-            empty_word_string += '_ '
+    print("\n" + empty_word_string.strip())
 
-        print("\n" + empty_word_string.strip())
+    pass
 
-        pass
 
-    @staticmethod
-    def guess_letter_message():
-        print("\nGuess a letter")
+def guess_letter_message():
+    print("\nGuess a letter")
 
-    @staticmethod
-    def show_available_letters(available_letters):
-        print('\n\t\t\t\tAvailable letters')
-        print(' '.join(available_letters))
 
-    @staticmethod
-    def show_word(word):
-        print(word)
+def show_available_letters(available_letters):
+    print('\n\t\t\t\tAvailable letters')
+    print(' '.join(available_letters))
 
-    @staticmethod
-    def show_correct_guessed_letters(word, guessed_letters):
-        guess_string = ''
 
-        for letter in word:
-            if letter in guessed_letters:
-                guess_string += ' ' + letter + ' '
-            else:
-                guess_string += '_ '
+def show_word(word):
+    print(word)
 
-        # remove leading and trailing white space
-        print(guess_string.strip())
+
+def show_correct_guessed_letters(word, guessed_letters):
+    guess_string = ''
+
+    for letter in word:
+        if letter in guessed_letters:
+            guess_string += ' ' + letter + ' '
+        else:
+            guess_string += '_ '
+
+    # remove leading and trailing white space
+    print(guess_string.strip())
